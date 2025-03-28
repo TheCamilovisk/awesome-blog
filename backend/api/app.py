0d8331424b986de5a17a096d1c9c6fd8913e4c3f
TheCamilovisk <camilolgon@gmail.com>
2025-03-28 20:42:02 -0300

    Add Message schema.

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,8 +1,12 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from api.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def get_root():
     return {'message': 'Hello, World!!!'}
